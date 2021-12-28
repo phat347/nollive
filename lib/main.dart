9f9d09909df0afbf337f941b1f8504760b00fcb0
Lê Khắc Trường Phát <phat@hatto.com>
2021-12-28 15:57:41 +0700

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:livekit_example/theme.dart';
+import 'package:logging/logging.dart';
+
+import 'pages/connect.dart';
+
+void main() {
+  // configure logs for debugging
+  Logger.root.level = Level.FINE;
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.message}');
+  });
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const LiveKitExampleApp());
+}
+
+class LiveKitExampleApp extends StatelessWidget {
+  //
+  const LiveKitExampleApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'LiveKit Flutter Example',
+        theme: LiveKitTheme().buildThemeData(context),
+        home: const ConnectPage(),
+      );
+}
