5ce4e302077dd031004299c70fc0fa6794e2c995
Hưng Nguyễn <>
2021-12-30 22:53:36 +0700

    - Command trust proxy

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ import 'pages/connect.dart';
 
 void main() {
   // Nếu bắt Charles thì bật
-  if (!kReleaseMode) {
-    // For Android devices you can also allowBadCertificates: true below, but you should ONLY do this when !kReleaseMode
-    final proxy = CustomProxy(
-        ipAddress: "192.168.0.138", port: 8888, allowBadCertificates: true);
-    proxy.enable();
-  }
+  // if (!kReleaseMode) {
+  //   // For Android devices you can also allowBadCertificates: true below, but you should ONLY do this when !kReleaseMode
+  //   final proxy = CustomProxy(
+  //       ipAddress: "192.168.0.138", port: 8888, allowBadCertificates: true);
+  //   proxy.enable();
+  // }
   // configure logs for debugging
   Logger.root.level = Level.FINE;
   Logger.root.onRecord.listen((record) {
